8680febff10e4a6ab6609f4edb8506e0f3338bae
Roberto Mazzalai <mazzalairob@gmail.com>
2021-12-05 15:57:36 +0100

    fixed script

--- script.sh
+++ script.sh
@@ -2,18 +2,22 @@
 
 if [ $# -eq 0 ]; then
  echo "no arguments, you should pass a year e.g. <2021>"
- read 
  exit 1
 fi
 
-i=0
+i=1
 dir=$1
+mkdir $dir
 while [ $i -le 25 ] 
 do
-    mkdir $dir"\day${i}"
-    touch $dir"\day${i}\solution.js"
+    if [[ ${#i} < 2 ]] 
+    then
+        mkdir $dir"\day0${i}"
+        touch $dir"\day0${i}\solution.js"
+    else
+        mkdir $dir"\day${i}"
+        touch $dir"\day${i}\solution.js"
+    fi
+    
     ((i++))
-    #let i=i+1
 done
-
-read 
